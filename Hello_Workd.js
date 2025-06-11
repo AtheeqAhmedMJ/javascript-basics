d4b59a262adb29f3e5ee4ca083ce611369cca519
Atheeq Ahmed <135537146+AtheeqAhmedMJ@users.noreply.github.com>
2025-06-11 19:24:50 +0530

    Create Hello_Workd.js

---
+++ Hello_Workd.js
@@ -0,0 +1,17 @@
+// console.log is a function used to print output to the console
+console.log("Hello World");
+console.log("Learning JS now!");
+
+/* Output will be
+Hello World
+Learning JS now!
+*/
+
+//Using a variable now
+
+let name = "World";
+console.log("Hello", name);
+
+/*
+OP/- Hello World
+*/
