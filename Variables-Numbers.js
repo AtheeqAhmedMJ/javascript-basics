1b709f22fd4e1536f12159cd8f98604698c3e5e7
Atheeq Ahmed <135537146+AtheeqAhmedMJ@users.noreply.github.com>
2025-06-11 22:50:10 +0530

    Create Variables-Numbers.js

---
+++ Variables-Numbers.js
@@ -0,0 +1,18 @@
+//in js, we don't have different dataypes like int and float to distinguish between decimals, and whole number
+//Instead we have one datatype, (maybe you guessed it) called numbers for everything
+
+let num1= 12733.0;
+let num2=3;
+
+let sum= num1*num2;
+let sum2= num1/num2;
+let sum3=sum+sum2;
+console.log(sum);
+console.log(sum2);
+console.log(sum3);
+console.log(typeof(sum)); //to check the type of dataype
+//o/p
+// 38199
+// 4244.333333333333
+// 42443.333333333336
+// number
