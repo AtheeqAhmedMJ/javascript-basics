9d4a3c7753ff3da63fb6fef04fcc29717089046e
Atheeq Ahmed <135537146+AtheeqAhmedMJ@users.noreply.github.com>
2025-06-11 20:05:26 +0530

    Create Variables-String.js

---
+++ Variables-String.js
@@ -0,0 +1,12 @@
+let stringname=`Atheeq`;
+console.log(stringname);
+
+// So basically, if u use sing quotes or double quotes, it is a string, but using backticks, i.e ``, is a little different, you can embed one thing in another
+
+let name="Ahmed";
+
+stringname= `Atheeq ${name}`;
+console.log(stringname);
+// O/p
+// Atheeq
+// Atheeq Ahmed
