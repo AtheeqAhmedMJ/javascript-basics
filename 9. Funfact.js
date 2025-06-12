e125920178ea6c272f069f717e9653407437063a
Atheeq Ahmed <135537146+AtheeqAhmedMJ@users.noreply.github.com>
2025-06-12 17:34:55 +0530

    Create 9. Funfact.js

---
+++ 9. Funfact.js
@@ -0,0 +1,5 @@
+//funfact, if u check the type of for null datatype, it shows object, even though null is considered a primitive datatype, it is a bug left uncorrected due to logistics in the official javascript documentation
+let x=null;
+console.log(typeof(x));
+// O/P
+// object
