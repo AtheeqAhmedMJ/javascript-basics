6eb1c0a348126127fada79fb46fc8585bc961f36
Atheeq Ahmed <135537146+AtheeqAhmedMJ@users.noreply.github.com>
2025-06-12 17:32:22 +0530

    Create 9. OtherVariables.js

---
+++ 9. OtherVariables.js
@@ -0,0 +1,26 @@
+//Boolean, works as same as other lang, either returns true or false
+let x=true;
+console.log(x);
+if(x){
+    console.log("Code is working");
+}
+//if statement works only when the value of boolean is true, else, it won't execute
+
+//next datatype in js, null and undefined
+
+let age=null;
+console.log(age);
+
+age=20;
+console.log(age);
+//this is used to declare the variable and attach the datatype later
+
+let y;
+//this is undefineded;
+console.log(typeof (y));
+// O/P
+//true
+// Code is working
+// null
+// 20
+// undefined
